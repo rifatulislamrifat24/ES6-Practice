a8c5544e05d88dc5be40cd5e9dd798e174c94096
rifatulislamrifat24 <rifatulislamrifat24@gmail.com>
2021-02-03 16:59:06 +0600

    let-const practice

---
+++ let-const.js
@@ -0,0 +1,20 @@
+const wife = "Purnima";
+console.log(wife);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+const nayok = {name:"Sakib Khan", phone:4578};
+
+// console.log(numbers);
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
+let sum = 0;
+// If variable is var then this code will run...
+// If variable is let then this code will give error..
+for(var i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
